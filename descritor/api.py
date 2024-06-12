3dbac73a96e908d10bc44ba727bc4a36c1cfb7da
Juan Mathews <91762249+azurejoga@users.noreply.github.com>
2024-06-12 15:36:41 -0300

    a

--- descritor/api.py
+++ descritor/api.py
@@ -78,4 +78,4 @@ def delete_request():
         return jsonify({"status": "error", "message": "Failed to delete request"}), response.status_code
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=5000)
